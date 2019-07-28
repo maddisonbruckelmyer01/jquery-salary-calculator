3049e675e8d755ebefbebe72aaace7ffe633635b
Maddison Bruckelmyer <maddisonbruckelmyer@Maddisons-MacBook-Air.local>
2019-07-28 15:45:42 -0500

    create delete button, and monthly total

--- client.js
+++ client.js
@@ -2,22 +2,27 @@ console.log('dom is loading');
 
 $(document).ready(readyNow); 
 
+let total = 0;
+let yearlyIncome = 0;
+
 function readyNow() {
     console.log('JQ');
     $('#grabInputs').on('click', inputTable);
-
-    
+    $('#grabInputs').on('click', monthlySalary);
+    $('#inputsForTable').on('click', '.deleteButton', deleteInputs);
 }//end readyNow
 
 function inputTable () {
-    console.log('working');
+    //console.log('working');
     $('#inputsForTable').append(`<tr>
     <td>${$('#firstName').val()}</td>
     <td>${$('#lastName').val()}</td>
     <td>${$('#idNumber').val()}</td>
     <td>${$('#jobTitle').val()}</td>
     <td>${$('#annualSalary').val()}</td>
+    <td><button class="deleteButton">Delete</button></td>
     </tr>`)
+   yearlyIncome += parseFloat($('#annualSalary').val());
     $('#firstName').val('');
     $('#lastName').val('');
     $('#idNumber').val('');
@@ -26,11 +31,21 @@ function inputTable () {
 }//end inputTable
 
 function monthlySalary () {
-   
+  // console.log('its working');
+   total = (yearlyIncome / 12);
+   console.log(total);
+   $('#money').text('Total Monthly: $ ' + total);
+   if (total > 20000) {
+       $('#money').css('background-color', 'red');
+       $('#money').css('color', 'white');
+   }
+
 }//end monthlySalary
 
-
-
+function deleteInputs() {
+    console.log('click');
+$(this).parent().parent().remove();
+}//end deleteInputs 
 
 
 
